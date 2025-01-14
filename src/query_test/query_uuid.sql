80172946f8f0542fa21beb39938c3d70ca99fd07
Taras Glek <taras@glek.net>
2025-01-14 14:21:13 +0200

    uuid test

--- src/query_test/query_uuid.sql
+++ src/query_test/query_uuid.sql
@@ -1,9 +1,9 @@
--- Manually pick a "l33t" UUID
 WITH l33t_uuid AS (
   SELECT 'deadbeef-1337-4b1d-8008-0123456789ab'::UUID AS uuid_value
 )
-SELECT uuid_value AS col1
-FROM l33t_uuid
-UNION ALL
-SELECT uuid_value::VARCHAR AS col1
+SELECT
+  uuid_value::VARCHAR AS uuid_string,
+  uuid_value AS uuid_uuid,
+  typeof(uuid_value::VARCHAR) AS uuid_string_type,
+  typeof(uuid_value) AS uuid_uuid_type
 FROM l33t_uuid;
