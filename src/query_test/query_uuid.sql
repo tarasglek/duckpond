6bd298bea5ccddabadae1c8dd08498aa4c861429
Taras Glek <taras@glek.net>
2025-01-14 14:18:27 +0200

    uuid is supported

--- src/query_test/query_uuid.sql
+++ src/query_test/query_uuid.sql
@@ -1,9 +1,9 @@
-WITH gen_uuid AS (
-  SELECT uuid() AS uuid_value
+-- Manually pick a "l33t" UUID
+WITH l33t_uuid AS (
+  SELECT 'deadbeef-1337-4b1d-8008-0123456789ab'::UUID AS uuid_value
 )
 SELECT uuid_value AS col1
-FROM gen_uuid
+FROM l33t_uuid
 UNION ALL
 SELECT uuid_value::VARCHAR AS col1
-FROM gen_uuid;
-
+FROM l33t_uuid;
