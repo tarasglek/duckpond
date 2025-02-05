960ba7c69f036985acc4123b2f12f70ba7e62ed7
Taras Glek <taras@glek.net>
2025-02-05 12:23:19 +0200

    chore: Add buildkitd container and output deployment to encrypted YAML

--- kraft/deploy.sh
+++ kraft/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # github-to-sops sops exec-env ../credentials/cloudflare-cf.r2.enc.json ./deploy.sh
 export UKC_METRO=fra0
+
+docker run -d --name buildkitd --privileged moby/buildkit:latest
+export KRAFTKIT_BUILDKIT_HOST=docker-container://buildkitd
+
 kraft cloud  deploy -g duckpond -M 2Gi \
             --rollout remove \
             -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
@@ -9,4 +13,7 @@ kraft cloud  deploy -g duckpond -M 2Gi \
            -e S3_BUCKET=$S3_BUCKET \
            -e S3_ENDPOINT=$S3_ENDPOINT \
            -e S3_PUBLIC_URL_PREFIX=$S3_PUBLIC_URL_PREFIX \
-            ..
+            .. \
+            -o json > /tmp/tempfile.json
+
+sops --output-type yaml -e /tmp/tempfile.json  -o > deployment.enc.yaml
