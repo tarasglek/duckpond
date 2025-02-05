bfdcb1b16cef1bc72b63fc2452552c429c59ce40
Taras Glek <taras@glek.net>
2025-02-05 15:59:15 +0200

    misc

--- src/duckpond.sh
+++ src/duckpond.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # convenient to ensure everything is recompiled
-pushd "$(dirname "$0")"
+pushd "$(dirname "$0")" > /dev/null
 go run $(ls *.go | grep -v '_test\.go$') $@
 popd > /dev/null
