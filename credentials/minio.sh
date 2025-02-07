9f00c1600fddbce67e60cef72496e28a7a1a212b
Taras Glek <taras@glek.net>
2025-02-07 12:12:26 +0200

    misc

---
+++ credentials/minio.sh
@@ -0,0 +1,7 @@
+export AWS_ACCESS_KEY_ID=demo
+export AWS_SECRET_ACCESS_KEY=demo-pass
+export S3_ENDPOINT=http://localhost:8883
+export S3_BUCKET=duckpond  # or your bucket name
+export S3_USE_PATH_STYLE=true
+export AWS_ALLOW_HTTP=true # for delta lake ext to use path-style routing
+export AWS_REGION=local
--- credentials/s3-env.sh
+++
@@ -1,6 +0,0 @@
- export AWS_ACCESS_KEY_ID=demo
- export AWS_SECRET_ACCESS_KEY=demo-pass
- export S3_ENDPOINT=http://localhost:8883
- export S3_BUCKET=duckpond  # or your bucket name
- export S3_USE_PATH_STYLE=true
- export AWS_REGION=local
